1336c30056566acd56b719ed2709e521f232f41d
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-03-26 18:55:03 +0545

    Remove unused namespace

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,19 +1,1 @@
-import { SpringValue } from "@react-spring/web";
-
 export type setState<T> = React.Dispatch<React.SetStateAction<T>>;
-
-export namespace SpringAnimationTypes {
-  export type OpacityAnimation = { opacity: SpringValue<number> };
-  export type LinearDisplacemenOpacityAnimation = {
-    x?: SpringValue<number>;
-    y?: SpringValue<number>;
-    opacity: SpringValue<number>;
-  };
-  export type ScaleAnimation = {
-    scale: SpringValue<number>;
-    transformOrigin?: string;
-  };
-  export type BouncingAnimation = {
-    y: SpringValue<number>;
-  };
-}
